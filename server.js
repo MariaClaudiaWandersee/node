0e7144437e672fa1b4379003cf6657c6cb36a08e
MariaClaudiaWandersee <mariaaclaudiaa2401@gmail.com>
2023-02-17 15:57:20 -0300

    inicio do JSON

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+http
+    .createServer((request, response) =>{
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        
+        response.end(JSON.stringify({
+            message: 'Minha primeira aplicação com nodeJS'
+        }));
+    })
+    .listen(3001, () => console.log("Servidor está rodando na porta 3001"));
